0b79b01c70e0a9b87c6e5d6f5b2e42e2dfa51076
Erik Lorenz <elor@hrz.tu-chemnitz.de>
2013-03-21 16:57:49 +0100

    excr: ald version merged back to actual upstream

--- excr/excr.cpp
+++ excr/excr.cpp
@@ -2,9 +2,8 @@
 #include <cstring>
 #include <sstream>
 #include <cstdlib>
+#include <cerrno>
 #include <stdexcept>
-
-#include <iostream>
 
 using namespace std;
 
@@ -19,18 +18,23 @@ excr::excr(char *input)
     throw runtime_error("Cycle data not found");
   }
 
-  cycle = atoi(it->second.c_str());
+  errno = 0;
+  char *p;
+  const char *str = it->second.c_str();
+  cycle = strtol(str, &p, 10);
+
+  if (errno != 0 || str == p)
+  {
+    throw runtime_error("excr: cycle number is no unsigned integer");
+  }
 
   it = props.find("Comment");
   if (it == props.end())
   {
-    comment = NULL;
+    throw runtime_error("Comment data not found");
   }
-  else
-  {
-    comment = new char[it->second.size() + 1];
-    strcpy(comment, it->second.c_str());
-  }
+  comment = new char[it->second.size() + 1];
+  strcpy(comment, it->second.c_str());
 
   it = props.find("Lattice");
   if (it == props.end())
@@ -72,17 +76,17 @@ void excr::createMap(char *input)
 
     if (tok[0] == '"')
     {
-      if (tok[strlen(tok)-1] != '"')
+      if (tok[strlen(tok) - 1] != '"')
       {
-        props[prop] += tok+1;
+        props[prop] += tok + 1;
         tok = strtok(NULL, "\"");
         props[prop] += " ";
         props[prop] += tok;
       }
       else
       {
-        tok[strlen(tok)-1] = '\0';
-        props[prop] += tok+1;
+        tok[strlen(tok) - 1] = '\0';
+        props[prop] += tok + 1;
       }
     }
     else
