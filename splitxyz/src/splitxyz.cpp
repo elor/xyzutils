2abc1dcab94d6ef3498172a5cee46ddc429f2820
Erik Lorenz <erik.e.lorenz@gmail.com>
2014-04-22 13:04:38 +0200

    splitxyz.cpp: options reworked

--- splitxyz/src/splitxyz.cpp
+++ splitxyz/src/splitxyz.cpp
@@ -2,7 +2,12 @@
 #include <fstream>
 #include <cstdlib>
 #include <cstring>
+#include <cassert>
 #include <ArgumentParser.h>
+
+#ifndef BUFFERSIZE
+  #define BUFFERSIZE 1024
+#endif
 
 using namespace std;
 
@@ -10,19 +15,18 @@ ArgumentParser args;
 
 void initArgs(int argc, char **argv)
 {
-  args.String("prefix", "", "filename prefix for output files", 'p');
-  args.UInt("step", 1, "step size between xyz frames.", 's');
-  args.UInt("first", 1, "first step to print", 'f');
-  args.UInt("digits", 0, "number of digits for output filename", 'd');
-  args.UInt("max", 0, "maximum number of output files. 0 = infinite", 'm');
+  args.String("output", "", "filename for output file(s). Leave empty for STDOUT. If it contains a series of '#', each step will be contained within its own file, where '#' is replaced with the step number", 'o');
+  args.UInt("step", 1, "step size", 's');
+  args.UInt("begin", 1, "first step to print. Starts at 1.", 'b');
+  args.UInt("end", 0, "last step to print. 0 = infinite", 'e');
   args.Standalones(
-      1,
+      -1,
       "input.xyz",
       "input file in xyz format. Please note that xyz files aren't properly validated prior to splitting.");
 
   args.parseArgs(argc, argv);
 
-  if (args.getStandaloneCount() != 1)
+  if (args.getStandaloneCount() < 0)
   {
     args.displayHelpMessage();
     exit(1);
@@ -33,64 +37,131 @@ int main(int argc, char **argv)
 {
   initArgs(argc, argv);
 
-  //        << " <file.xyz> [prefix] [stepsize] [firststep] [digits] [maxfiles]"
+  char outfile_format[BUFFERSIZE];
+  memset(outfile_format, '\0', BUFFERSIZE);
+  const char *outfile_argument = args.getCString("output");
+  bool useSTDOUT = outfile_argument == NULL || strlen(outfile_argument) == 0;
 
-  const char *prefix = args.getCString("prefix");
+  size_t digits = 0;
+  bool useMultipleFiles = false;
+  if (!useSTDOUT)
+  {
+    size_t length = strlen(outfile_argument);
+    for (size_t src = 0, trg = 0; src < length; src++, trg++)
+    {
+      if (outfile_argument[src] == '%')
+      {
+        cerr << "output filename must not contain '%'" << endl;
+        exit(1);
+      }
+
+      while (outfile_argument[src] == '#')
+      {
+        if (!useMultipleFiles)
+        {
+          static const char *anchor = "%0*lu";
+          strcpy(&outfile_format[trg], anchor);
+          trg += strlen(anchor);
+          useMultipleFiles = true;
+        }
+        digits++;
+        src++;
+      }
+      
+      outfile_format[trg] = outfile_argument[src];
+    }
+  }
+
   size_t stepsize = args.getUInt("step");
-  size_t firststep = args.getUInt("first");
-  size_t digits = args.getUInt("digits");
-  size_t maxfiles = args.getUInt("max");
-  bool usemaxfiles = maxfiles != 0;
-  const char *filename = args.getCStandalone(0);
+  if (stepsize == 0)
+  {
+    stepsize = 1;
+  }
+  size_t firststep = args.getUInt("begin");
+  if (firststep == 0)
+  {
+    firststep = 1;
+  }
+  size_t laststep = args.getUInt("end");
+  bool useLastStep = (laststep != 0);
+  int numFiles = args.getStandaloneCount();
+  bool useSTDIN = numFiles == 0;
+  char filename[BUFFERSIZE];
+  memset(filename, '\0', BUFFERSIZE);
+  if (!useSTDIN)
+  {
+    strcpy(filename, args.getCStandalone(0));
+    if (numFiles > 1)
+    {
+      cerr << "multiple input files not yet supported" << endl;
+      exit(1);
+    }
+  }
 
+  // TODO: also read from stdin
+  assert(filename != NULL);
   ifstream file(filename);
+
+  assert(file.is_open());
 
   size_t atomsleft = 0;
   size_t step = 0;
   size_t actualstep = 0;
-  char line[1024];
-  char outfilename[1024];
+  char line[BUFFERSIZE];
+  char outfilename[BUFFERSIZE];
 
-  ofstream out;
+  ostream *out = NULL;
 
-  while (!file.eof())
+  if (useSTDOUT)
   {
-    file.getline(line, 1024);
+    out = &cout;
+  }
+  else if (!useMultipleFiles)
+  {
+    strcpy(outfilename, outfile_format);
+    out = new ofstream(outfilename);
+  }
+
+  bool printLine = false;
+
+  while (file.is_open() && !file.eof())
+  {
+    file.getline(line, BUFFERSIZE);
 
     if (atomsleft == 0)
     {
-      if (file.eof())
+      printLine = false;
+
+      ++step;
+      if (useLastStep && step > laststep)
       {
-        cout << "end of file reached" << endl;
         break;
       }
-      ++step;
-      out.close();
+
+      if (useMultipleFiles && !useSTDOUT)
+      {
+        delete out;
+        out = NULL;
+      }
       atomsleft = atoi(line) + 2;
 
-      if (firststep <= 1)
+      if (step >= firststep)
       {
         if (actualstep % stepsize == 0)
         {
-          if (usemaxfiles)
+          if (useMultipleFiles)
           {
-            if (maxfiles == 0)
-            {
-              break;
-            }
-            --maxfiles;
-          }
+            assert(out == NULL);
 
-          sprintf(outfilename, "%s%0*lu.xyz", prefix, (int) digits, step);
-          out.open(outfilename);
-          cout << "writing step " << step << " to file " << outfilename << endl;
+            sprintf(outfilename, outfile_format, (int) digits, step);
+            cerr << outfilename << endl;
+            out = new ofstream(outfilename);
+          }
+//          cout << "writing step " << step << " to file " << outfilename << endl;
+          printLine = true;
         }
 
         ++actualstep;
-      }
-      else
-      {
-        --firststep;
       }
 
       //      if (!out.is_open())
@@ -99,11 +170,18 @@ int main(int argc, char **argv)
       //      }
     }
 
-    if (out.is_open())
+    if (printLine)
     {
-      out << line << endl;
+      assert(out != NULL);
+      *out << line << endl;
     }
     --atomsleft;
+  }
+
+  if (out != NULL && !useSTDOUT)
+  {
+    delete out;
+    out = NULL;
   }
 
   return 0;
